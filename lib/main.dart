d8535d232871877f3e316b694497c9ae1931cf69
orbachinujbuk <orbachinujbuk@gmail.com>
2021-01-17 23:33:15 +0600

    AS324324

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+        // appBar: AppBar(
+        //   title: Text('Welcome to Flutter'),
+        // ),
+        body: WebView(
+          initialUrl: "https://sis.fantasygeek11.com/dashboard",
+          javascriptMode: JavascriptMode.unrestricted,
         ),
       ),
     );
