f8c6daddf27d52a3d19fbc09ab735c339b94a86e
orbachinujbuk <orbachinujbuk@gmail.com>
2021-01-18 00:23:44 +0600

    ASD234

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import '../globals.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+  _showSnackbar(String textForSnackbar) {
+    var _mySnackbar = SnackBar(
+      content: Text(textForSnackbar),
+    );
+    _globalKey.currentState.showSnackBar(_mySnackbar);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +26,7 @@ class MyApp extends StatelessWidget {
           child: WebView(
             initialUrl: "https://sis.fantasygeek11.com/dashboard",
             javascriptMode: JavascriptMode.unrestricted,
+            onWebResourceError: ,
           ),
         ),
       ),
